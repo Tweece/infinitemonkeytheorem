91c60e4c3d97b683f7f965344038f2c357a830bc
tweece <kiddiestrangler@protonmail.com>
2021-09-08 19:50:15 +0200

    Added comments and stuff

--- infinitemonkeytheorem.c
+++ infinitemonkeytheorem.c
@@ -5,36 +5,42 @@
 
 void randomizeAndFind(char **pointersToPartitions, float numberOfGigs, char *phrase) {
 	int phraseLength = 1, i;
-	char* layout = "QDRWBJFUPASHTGYNEOIZXMCVKL ,.?!"
-	while(*(phrase+1) != '\0') {
-		phrase++;
-		phraseLength++;
+	char* layout = "QDRWBJFUPASHTGYNEOIZXMCVKL ,.?!";
+	while(*(++phrase) != '\0') { // checking the length of the phrase 
+		phraseLength++; 
+	} 
+	for(i = 1; i < phraseLength; i++) { // generating first phraseLength-1 cause we are checking backwards and we don't want to go before the start of the array
+		**pointersToPartitions = layout[rand()%31];
+		*pointersToPartitions++;
 	}
-	for (i = 0; i > phraseLength; i++) {
-		
-		**pointersToPartitions;
+	while(*(++(*pointersToPartitions)) != '\0') {	
+		// stuff needs to ge here
 	}
 }
 
 
 int main() {
+//// user input prototype ////
 	char* phrase = "abbss"
 	float numberOfGigs = 2.6f;
+	float numberOfGigsRefrence = numberOfGigs;
+//// randomization seed ////
 	time_t t;
 	srand((unsigned) time(&t));
-	float numberOfGigsReference = numberOfGigs;
+//// partitioning system ////
 	int i = 0;
-	int numberOfPartitions = (int) (numberOfGigs + 1.0f);
-	char **pointersToPartitions = char(**)malloc(numbersOfPartitions * sizeof(char*));
-	if(numberOfGigs >= 1.0f) {
+	int numberOfPartitions = (int) (numberOfGigs + 0.999f); // getting the amount of partitions
+	char **pointersToPartitions = char(**) malloc(numbersOfPartitions * sizeof(char*)); // allocating the partition array
+	if(numberOfGigs >= 1.0f) { // check if a 1 gB partition can be assigned 
 		do {
-			*pointersToPartitions = (char *)malloc(1000000000 * sizeof(char));
-			randomizeAndFind(char **pointersToPartitions, float numberOfGigs, char* phrase)
-			printf("Processing %dGB out of %0.3fGB\n", ++i, numberOfGigsReference);
-		} while(--numberOfGigs >= 1.0);
+			*pointersToPartitions = (char *)malloc(1000000000 * sizeof(char)); // allocating space for a 1 gB partiion
+			randomizeAndFind(char **pointersToPartitions, float numberOfGigs, char* phrase) // running the randomization and finiding 
+			printf("Processing %dGB out of %0.3fGB\n", ++i, numberOfGigsReference); // loading indicator
+		} while(--numberOfGigs >= 1.0); 
 	}
-	if(numberOfGigs != 0) {
-		printf("Processing %0.3fGB out of %0.3fGB\n", numberOfGigs, numberOfGigsReference);
+	if(numberOfGigs != 0) { // creating a partition with the rest of the memory left
+		printf("Processing %0.3fGB out of %0.3fGB\n", numberOfGigs, numberOfGigsReference); // another loading indicator
 	}
+//// partitioning ends ////
 	return 0;
 }
