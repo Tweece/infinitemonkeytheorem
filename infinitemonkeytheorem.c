35236fa666f62ace7e50bbbdceaeec30c1bce22c
tweece <kiddiestrangler@protonmail.com>
2021-09-11 10:07:52 +0200

    Close to the end, requires saving the testing and saving found phrases

--- infinitemonkeytheorem.c
+++ infinitemonkeytheorem.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
 
-void randomizeAndFind(char **pointersToPartitions, float numberOfGigs, char *phrase) {
-	int phraseLength = 1, i;
+void randomizeAndFind(char **pointersToPartitions, float numberOfGigs, char *phraseBackwards) {
+	int i, phraseLength = 1, counter;
 	char* layout = "QDRWBJFUPASHTGYNEOIZXMCVKL ,.?!";
-	while(*(++phrase) != '\0') { // checking the length of the phrase 
-		phraseLength++; 
-	} 
-	for(i = 1; i < phraseLength; i++) { // generating first phraseLength-1 cause we are checking backwards and we don't want to go before the start of the array
+	char* temp1;
+	char* temp2;
+	while(*(++phraseBackwards) != '\0') phraseLength++; // checking the length of the phrase
+	phraseBackwards--; // making it point to the last character of the array rather than the null byte
+	for(i = 1; i < phraseLength; i++) { // generating the first phraseLength-1 number of characters cause we are checking backwards and we don't want to go before the start of the array
 		**pointersToPartitions = layout[rand()%31];
 		*pointersToPartitions++;
 	}
-	while(*(++(*pointersToPartitions)) != '\0') {	
-		// stuff needs to ge here
+	while(*(++(*pointersToPartitions)) != '\0') {
+		**pointersToPartitions = layout[rand()%31]; 
+		// initializing the backwards search
+		if **pointersToPartitions == *phraseBackwards {
+			// saving and changing pointers
+			temp1 = *pointersToPartitions; temp2 = phraseBackwards; i = 1; (*pointersToPartitions)--; phraseBackwards--; counter = 1;
+			// actual backwards search
+			while(counter < phraseLength;) {
+				if(**pointersToPartitions != *phraseBackwards) break;
+				(*pointersToPartitions)--; phraseBackwards--; i++; counter++;
+			}
+			*pointersToPartitions = temp1; phraseBackwards = temp2;
+			if(i==phraseLength) {} // save the address of *pointersToPartitons cause it found our phrase
+		}
 	}
 }
 
